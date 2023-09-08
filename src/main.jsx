c8e249b6f2c0dd4567a1f91558610c912e54aeea
jongbeeq <titivutewhangsukij@gmail.com>
2023-09-08 10:59:27 +0700

    context

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,69 @@
-import React from 'react'
+import React, { useState, createContext, useContext } from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+// Context
+// 1. createContext [Provider,Consumer] => ชื่อ Context
+const ThemeContext = createContext();
+console.log(ThemeContext);
+
+// ###A1. สร้าง HOC : Higher Order Component [Provider]
+// HOC คือ FC ที่รับ Component เข้าไป แล้ว return component ใหม่ออกมา
+// function ThemeContextProvider(props) {
+//   console.log(props.children)
+//   return <div>Hi {props.children}</div>;
+// }
+
+
+/* ###A2. Share Data & Logic ผ่าน attribute value
+==> Data (state,boolean,string,object,array,etc.)
+==> Logic (Fn ที่ใช้ handle ต่างๆ)
+*/
+
+// Data : isDarkMode,styleObj
+// Logic : setIsDarkMode,handleToggleTheme
+function ThemeContextProvider(props) {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const stylesObj = {
+    backgroundColor: isDarkMode ? "black" : 'white',
+    color: isDarkMode ? "white" : 'black'
+  };
+  const handleToggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+  const shareObj = { theme: stylesObj, toggleTheme: handleToggleTheme };
+  return <ThemeContext.Provider value={shareObj}>{props.children}</ThemeContext.Provider>;
+}
+
+/* ###A3. นำ Provider ไปครอบ Children [Provider]
+  <ThemeContextProvider>
+    <App />
+  </ThemeContextProvider>
+*/
+
+/* ### B1 : ดึงค่า Shared object ผ่านตัว useContext
+SYNTAX : useContext(ContextName)
+
+ex. const sharedObj = useContext(ThemeContext)
+*/
+
+
+// ###########################################
+// ###########################################
+// ###########################################
+
+// UI
+function App() {
+  const s = useContext(ThemeContext)
+  console.log(s);
+
+  return <div className="App" style={s.theme}>
+    <h1>Theme App</h1>
+    <button onClick={s.toggleTheme}>Toggle Theme</button>
+  </div>;
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <div>Vite React Template</div>
+  <ThemeContextProvider>
+    <App />
+  </ThemeContextProvider>
 )
