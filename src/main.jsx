d7a628ba04e8fb32e28915260beb0b337fa4c8db
jongbeeq <titivutewhangsukij@gmail.com>
2023-09-08 11:50:06 +0700

    login with interface api

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,53 @@
-import React from 'react'
+import React, { useState, createContext, useContext, useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+// AuthContext
+const AuthContext = createContext();
+
+function AuthContextProvider(props) {
+  const [isAuth, setIsAuth] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+
+  // isAuth : false => true ให้ delay 1 วิ
+  // useEffect(() => {
+  //   if (isLoading) {
+  //     setTimeout(() => {
+  //       setIsLoading(false);
+  //     }, 3000)
+  //   }
+  // }, [isAuth]);
+
+  const handleAuth = () => {
+    // logun : isAuth : false => true
+    if (!isAuth) {
+      setIsLoading(true);
+      setTimeout(() => {
+        setIsAuth(true);
+        setIsLoading(false);
+      }, 3000)
+    } else {
+      setIsAuth(false);
+    }
+  };
+  return <AuthContext.Provider value={{ isAuth, handleAuth, isLoading }}>{props.children}</AuthContext.Provider>
+}
+
+function App() {
+  const { isAuth, handleAuth, isLoading } = useContext(AuthContext)
+  return (
+    <div className='App'>
+      {isLoading ? <h1>Loading...</h1> : <h1>Wlcome... {!isAuth ? 'Guest' : 'User'}</h1>}
+      <button onClick={handleAuth} disabled={isLoading}>{!isAuth ? 'Login' : 'Logout'}</button>
+    </div>
+  );
+};
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <div>Vite React Template</div>
+  <AuthContextProvider>
+    <App />
+  </AuthContextProvider>
 )
+
+{/* <h3>Welcome.. Guest</h3>
+<button>Login</button> */}
